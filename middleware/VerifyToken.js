04b4ca4325edfbca255e05adf0316801d2750f0a
Shedrack Okunda <okundashedrack@gmail.com>
2025-01-03 16:08:41 +0300

    Created a middleware folder and created a file to verify token

---
+++ middleware/VerifyToken.js
@@ -0,0 +1,40 @@
+import jwt from 'jsonwebtoken'
+import { sanitizeUser } from '../utils/SanitizeUser'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const verifyToken = async (req, res, next) => {
+    try {
+        // extract the token from request cookies
+        const { token } = req.cookies
+
+        // if token is not there return 401 response
+        if (!token) {
+            return res.status(401).json({message: 'Token missing, please login again.'})
+        }
+
+        // verifies the token
+        const decodedInfo = jwt.verify(token, process.env.SECRET_KEY)
+
+        // checks if decoded info contains legit details, then set that info in req.user and calls next
+        if (decodedInfo && decodedInfo._id && decodedInfo.email) {
+            req.user = decodedInfo
+            next()
+        }
+
+        // if token is invalid then sends the response accordingly
+        else {
+            return res.status(401).json({message: 'Invalid token, please login again.'})
+        }
+    } catch (error) {
+        console.log(error)
+
+        if (error instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({message: 'Token expired, please login again.'})
+        } else if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(401).json({message: 'Invalid token, please login again.'})
+        } else {
+            return res.status(500).json({message: 'Internal server error.'})
+        }
+    }
+}
